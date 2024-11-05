7b71873d1f2c141a08637d1fe34d5c3cf1acb83c
jesus <jesusflsa2004@gmail.com>
2024-11-05 12:30:30 -0500

    update: enable caching

--- src/main/java/com/jesusfs/tienda/TiendaApplication.java
+++ src/main/java/com/jesusfs/tienda/TiendaApplication.java
@@ -2,8 +2,10 @@ package com.jesusfs.tienda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TiendaApplication {
 
 	public static void main(String[] args) {
